da446bd9cf1af52d2f8c667c2eedcd47121af944
Giovanni <g.salmeri@mondodomani.org>
2020-04-29 21:12:48 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# yellow-replain
-Show Re:plain live chat
+Replain 0.8.10
+=============
+Show Re:plain live chat.
+
+<p align="center"><img src="replain-screenshot.png?raw=true" width="795" height="836" alt="Screenshot"></p>
+
+## How to install extension
+
+1. [Download and install Datenstrom Yellow](https://github.com/datenstrom/yellow/).
+2. [Download extension](../../archive/master.zip). If you are using Safari, right click and select 'Download file as'.
+3. Copy `master.zip` into your `system/extensions` folder.
+
+To uninstall delete the [extension files](extension.ini).
+
+## How to show a chat
+
+[Re:plain](https://replain.cc/) is a chat service for websites. To use this extension open file system/settings/system.ini and set the value of `ReplainId`. You can find the id of your chat in the second line of the code provided by the Re:plain bot. The chat is shown on every page.
+
+This extension uses an online service and requires on the operator side a Telegram, WhatsApp or Facebook account. You can configure the chat with the Re:plain bot.
+
+## Settings
+
+The following setting must be configured in file system/settings/system.ini:
+
+`ReplainId` = your Re:plain id
+
+## Developer
+
+Giovanni Salmeri. [Get support](https://github.com/GiovanniSalmeri/yellow-replain/issues).
---
+++ replain.php
@@ -0,0 +1,32 @@
+<?php
+// Replain extension, https://github.com/GiovanniSalmeri/yellow-replain
+// Copyright (c) 2020 Giovanni Salmeri
+// This file may be used and distributed under the terms of the public license.
+
+class YellowReplain {
+    const VERSION = "0.8.10";
+    const TYPE = "feature";
+    public $yellow;         //access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+        $this->yellow->system->setDefault("replainId", "########-####-####-####-############");
+    }
+    
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output .= null;
+        if ($name == "header") {
+            $output .= "<script>\n";
+            $output .= "document.addEventListener('DOMContentLoaded', function() {\n";
+            $output .= "window.replainSettings = { id: '".$this->yellow->system->get("replainId")."' };\n";
+            $output .= "(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;\n";
+            $output .= "var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);\n";
+            $output .= "})('https://widget.replain.cc/dist/client.js');\n";
+            $output .= "});\n";
+            $output .= "</script>\n";
+        }
+        return $output;
+    }
+}
